eb37b5fc8dcc3e825e662862374b0c3d3eefeedc
mayank <shrivastava00mayank@gmail.com>
2025-02-09 16:31:20 -0500

    pylint

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -3,11 +3,31 @@ import json
 
 
 def emotion_detector(text_to_analyze):
+    """Analyzes emotion of text_to_analyze"""
+    # url, headers and  data
     URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyze } }
+
+    # fetch response from watsonx
     resp = requests.post(url=URL, headers=headers, data=json.dumps(input_json))
-    resp = json.loads(resp.text)['emotionPredictions'][0]['emotion']
-    dominant = sorted(resp.items(), key=lambda x: x[1], reverse=True)
-    resp['dominant_emotion'] = dominant[0][0]
-    return resp
+
+    # Get status code
+    print(resp.status_code)
+    if resp.status_code == 200:
+        response = json.loads(resp.text)['emotionPredictions'][0]['emotion']
+        dominant = sorted(response.items(), key=lambda x: x[1], reverse=True)
+        response['dominant_emotion'] = dominant[0][0]
+    
+    # Status code 500
+    elif resp.status_code == 400:
+        response = {
+            "anger": None,
+            "disgust": None, 
+            "fear": None, 
+            "joy": None, 
+            "sadness": None, 
+            "dominant_emotion": None
+            }
+   
+    return response
--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+"""server.py file"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -6,16 +8,23 @@ app = Flask("Emotion Detection")
 
 @app.route("/")
 def render_index_page():
+    """Generates the template using index.html"""
     return render_template('index.html')
 
 
 @app.route("/emotionDetector")
 def emotion_analyzer():
+    """Detects emotion of input text using emotion_detector function"""
+
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
     # Pass the text to the emotion_deteector function and store the response
-    response = emotion_detector(text_to_analyze)    
+    response = emotion_detector(text_to_analyze)
+
+    # Case where dominant_emotion is None
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!."
 
     # Return a formatted string with the emotion score and dominant emotion.
     return "For the given statement, the system response is " + \
